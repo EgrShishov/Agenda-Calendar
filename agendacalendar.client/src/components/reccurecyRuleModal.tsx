d716e10e2048482b6eba65a144a12cbc79566aef
EgrShishov <e.shishov98@yandex.ru>
2024-05-08 21:33:05 +0300

    Update components

--- agendacalendar.client/src/components/reccurecyRuleModal.tsx
+++ agendacalendar.client/src/components/reccurecyRuleModal.tsx
@@ -3,11 +3,9 @@ import {
     TextField,
     Select,
     MenuItem,
-    InputLabel,
     FormControl,
-    Button,
 } from '@mui/material';
-import {ReccurencyDate, ReccurencyRule} from "../models/reccurencyRulePatternModel.ts";
+import {ReccurencyRule} from "../models/reccurencyRulePatternModel.ts";
 import {enGB} from "date-fns/locale";
 import {DatePicker} from "react-nice-dates";
 
@@ -18,7 +16,7 @@ const ReccurecyRuleModal = ({setShowReccurenceModal}) => {
         setRepeatInterval(event.target.value);
     }
 
-    const [repeatIntervalType, setRepeatIntervalType] = useState('');
+    const [repeatIntervalType, setRepeatIntervalType] = useState(1); //days
 
     const handleRepeatIntervalTypeChange = (event) => {
         setRepeatIntervalType(event.target.value);
@@ -74,7 +72,7 @@ const ReccurecyRuleModal = ({setShowReccurenceModal}) => {
         event.preventDefault();
         const rec_rule: ReccurencyRule = {
             frequency: frequency,
-            interval: interval,
+            interval: repeatIntervalType * repeatIntervalValue,
             daysOfWeek: daysOfWeek,
             daysOfMonth: daysOfMonth,
             weeksOfMonth: weeksOfMonth,
@@ -124,15 +122,15 @@ const ReccurecyRuleModal = ({setShowReccurenceModal}) => {
                                     onChange={handleRepeatIntervalTypeChange}
                                     className="mr-2 w-32"
                                 >
-                                    <MenuItem value="days">дни</MenuItem>
-                                    <MenuItem value="weeks">недели</MenuItem>
-                                    <MenuItem value="months">месяцы</MenuItem>
-                                    <MenuItem value="years">годы</MenuItem>
+                                    <MenuItem value={1}>дни</MenuItem>
+                                    <MenuItem value={7}>недели</MenuItem>
+                                    <MenuItem value={12}>месяцы</MenuItem>
+                                    <MenuItem value={365}>годы</MenuItem>
                                 </Select>
                             </div>
                         )}
 
-                        {repeatIntervalType !== 'days' && (
+                        {repeatIntervalType !== 1 && (
                             <div className="flex flex-col">
                                 <span className="text-gray-500 text-ms font-bold">Days of week:</span>
                                 <div className="flex flex-wrap justify-center">
@@ -149,41 +147,92 @@ const ReccurecyRuleModal = ({setShowReccurenceModal}) => {
                                 </div>
                             </div>
                         )}
-                        {repeatInterval !== 'none' && (
-                            <div className="">
-                                <span
-                                    className=""
-                                >
-                                    Окончание</span>
-                                <Select
-                                    labelId="repeat-end-label"
-                                    id="repeat-end"
-                                    value={repeatEndType}
-                                    onChange={handleRepeatEndTypeChange}
-                                    className="w-full mb-4"
-                                >
-                                    <MenuItem value="never">Никогда</MenuItem>
-                                    <MenuItem value="date">Дата</MenuItem>
-                                </Select>
-                                {repeatEndType === 'date' && (
-                                    <div>
-                                        <span>
-                                            Дата окончания:
-                                        </span>
-                                        <TextField
-                                            id="repeat-end-date"
-                                            type="date"
-                                            value={repeatEndDate}
-                                            onChange={handleRepeatEndDateChange}
-                                            className="w-full mb-4"
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
-                                        />
+                        <div className="row-span-1 flex items-center">
+                            <div className="grid-cols-1/4 items-end gap-y-3.5">
+                                {repeatInterval !== 'none' && (
+                                    <div className="row-span-1 flex items-center gap-x-2.5">
+                                        <div>
+                                             <span
+                                                 className="text-gray-400 font-semibold text-ms"
+                                             >
+                                                Начиная с
+                                             </span>
+                                            <Select
+                                                labelId="repeat-end-label"
+                                                id="repeat-end"
+                                                value={repeatStartType}
+                                                onChange={handleRepeatStartTypeChange}
+                                                className="w-full mb-2"
+                                            >
+                                                <MenuItem value="never">Никогда</MenuItem>
+                                                <MenuItem value="date">Дата</MenuItem>
+                                            </Select>
+                                        </div>
+                                        {repeatStartType === 'date' && (
+                                            <div>
+                                                <span className="text-gray-400 font-semibold text-ms">
+                                                    Дата начала:
+                                                </span>
+                                                <DatePicker
+                                                    date={repeatStartDate}
+                                                    onDateChange={handleRepeatStartDateChange}
+                                                    format={"yyyy-MM-dd"}
+                                                    locale={enGB}>
+                                                    {({ inputProps, focused }) => (
+                                                        <input
+                                                            className={'input' + (focused ? ' -focused' : '')}
+                                                            {...inputProps}
+                                                            style={{ backgroundColor: "transparent"}}
+                                                            placeholder="Select start time"
+                                                        />
+                                                    )}
+                                                </DatePicker>
+                                            </div>
+                                        )}
+                                    </div>
+                                )}
+                                {repeatInterval !== 'none' && (
+                                    <div className="row-span-1 flex items-center gap-x-2.5">
+                                        <div>
+                                             <span
+                                                 className="text-gray-400 font-semibold text-ms"
+                                             >
+                                                Заканчивая
+                                             </span>
+                                            <Select
+                                                value={repeatEndType}
+                                                onChange={handleRepeatEndTypeChange}
+                                                className="w-full mb-2"
+                                            >
+                                                <MenuItem value="never">Никогда</MenuItem>
+                                                <MenuItem value="date">Дата</MenuItem>
+                                            </Select>
+                                        </div>
+                                        {repeatEndType === 'date' && (
+                                            <div>
+                                                <span className="text-gray-400 font-semibold text-ms">
+                                                    Дата окончания:
+                                                </span>
+                                                <DatePicker
+                                                    date={repeatEndDate}
+                                                    onDateChange={handleRepeatEndDateChange}
+                                                    format={"yyyy-MM-dd"}
+                                                    locale={enGB}>
+                                                    {({ inputProps, focused }) => (
+                                                        <input
+                                                            className={'input' + (focused ? ' -focused' : '')}
+                                                            {...inputProps}
+                                                            style={{ backgroundColor: "transparent"}}
+                                                            placeholder="Select end date"
+                                                        />
+                                                    )}
+                                                </DatePicker>
+                                            </div>
+                                        )}
                                     </div>
                                 )}
                             </div>
-                        )}
+                        </div>
                     </FormControl>
                     <div className="row-span-1 flex items-center justify-end gap-3">
                         <button
